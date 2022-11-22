c9f561ecf62242b4114754d8cb27528154bc184f
Harsha <harshadatla@gmail.com>
2022-11-22 16:19:49 -0600

    Adding new create react app with native base

---
+++ my-app/src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Box, Heading } from "native-base";
+
+const Main = () => {
+	return (
+		<Box bg="primary.400" p="3" rounded="lg">
+			<Heading textAlign={"center"} textTransform={"capitalize"}>
+				Welcome
+			</Heading>
+		</Box>
+	);
+};
+
+export default Main;
